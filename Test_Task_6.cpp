ad3b24ae32ee0b6f83c61e1f6b94e4bba17d6209
moru1234 <moreshwarinamdar123@gmail.com>
2024-12-23 15:18:49 +0530

    Task: Test proper initialization of constructors and destructors.

---
+++ Test_Task_6.cpp
@@ -0,0 +1,43 @@
+
+#include <gtest/gtest.h>
+#include<string>
+using namespace std;
+
+//Task: Test proper initialization of constructors and destructors.
+
+// Class Datalogger - create constructor initialize with - "Data Logg constructor"
+//                  - Create Destructor - "Data logger destructor"
+
+
+class Datalogger
+{
+private:
+
+public:
+    string log;
+    Datalogger():log ("Data Logg constructor")
+    {
+
+    }
+    ~Datalogger()
+    {
+        log = "Data Logg Destructor";
+    }
+};
+
+
+
+TEST(ConstructorDestructorTest, subTestConstructor)
+{
+    //Datalogger log1;
+    Datalogger* log1 = new Datalogger;
+
+    EXPECT_EQ(log1->log, "Data Logg constructor");
+}
+
+
+// Main function for running tests
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
